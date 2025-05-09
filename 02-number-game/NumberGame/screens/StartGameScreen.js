06d7192d8b8b5aa135e1f312f81d3a84425f5ea3
NiramayThaker <thakerniramay@gmail.com>
2025-05-10 00:51:46 +0530

    Creating Textinput rules

--- 02-number-game/NumberGame/screens/StartGameScreen.js
+++ 02-number-game/NumberGame/screens/StartGameScreen.js
@@ -5,7 +5,13 @@ import PrimaryButton from '../components/PrimaryButton'
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-        <TextInput />
+        <TextInput 
+            style={styles.numberInput} 
+            maxLength={2} 
+            keyboardType="number-pad"
+            autoCapitalize='none'
+            autoCorrect={false}
+        />
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -22,6 +28,26 @@ const styles = StyleSheet.create({
         padding: 16,
         backgroundColor: '#72063c',
         borderRadius: 8,
-        elevation: 5, // For android
+
+        // For android box shadow
+        elevation: 5, 
+
+        // For IOS box shadow
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: .25
+    },
+
+    numberInput: {
+        height: 70,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold', 
+        textAlign: 'center',
     }
 })
