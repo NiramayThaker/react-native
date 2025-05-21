f38eb45818b7c5bdb32a4ad7d42dc305cc30f365
NiramayThaker <thakerniramay@gmail.com>
2025-05-22 03:16:08 +0530

    Styling grid based layout

--- 03-navigation-app/starting-project/components/CategoryGridTile.js
+++ 03-navigation-app/starting-project/components/CategoryGridTile.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Pressable, Text, View } from 'react-native'
+import { Pressable, Text, View, StyleSheet } from 'react-native'
 
 function CategoryGridTile({title, color}) {
   return (
-    <View>
-        <Pressable>
-            <View>
-                <Text>
+    <View style={[styles.gridItem, {backgroundColor: color}]}>
+        <Pressable 
+            android_ripple={{'color': '#ccc'}}
+            style={({pressed}) => [styles.button, pressed ? styles.buttonPressed : null]}
+        >
+            <View style={styles.innerContainer}>
+                <Text style={styles.title}>
                     {title}
                 </Text>
             </View>
@@ -16,3 +19,41 @@ function CategoryGridTile({title, color}) {
 }
 
 export default CategoryGridTile
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        elevation: 4,
+
+        backgroundColor: 'white',
+        shadowColor: 'black',
+        shadowOpacity: .25,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 8,
+
+        overflow: 'hidden',
+    },
+
+    button: {
+        flex: 1,
+    },
+
+    buttonPressed: {
+        opacity: .5,
+    },
+
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    }
+})
