ddad28c783eca2eecbc6696b9762ac423aeeb175
NiramayThaker <thakerniramay@gmail.com>
2025-05-22 02:29:05 +0530

    Creating Categories Screen

---
+++ 03-navigation-app/starting-project/components/CategoryGridTile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Pressable, Text, View } from 'react-native'
+
+function CategoryGridTile({title, color}) {
+  return (
+    <View>
+        <Pressable>
+            <View>
+                <Text>
+                    {title}
+                </Text>
+            </View>
+        </Pressable>
+    </View>
+  )
+}
+
+export default CategoryGridTile
